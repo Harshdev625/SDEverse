c172403b45490579d44309562aea45a7d8239f58
Surya P R <suryaravichandran5555@gmail.com>
2025-10-13 02:09:32 +0530

    added dark mode toggle with Redux state and Tailwind support

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,19 +1,27 @@
 import { Outlet, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
 import Sidebar from "./Sidebar";
 import Footer from "./Footer";
 
 const Layout = () => {
   const location = useLocation();
+  const themeMode = useSelector((state) => state.theme.mode);
 
   const isHomePage = location.pathname === "/";
 
   return (
-    <div className="flex min-h-screen bg-gray-100 dark:bg-gray-950 text-gray-900 dark:text-white">
+    <div 
+      className="flex min-h-screen"
+      style={{
+        backgroundColor: themeMode === 'dark' ? '#030712' : '#f3f4f6',
+        color: themeMode === 'dark' ? '#ffffff' : '#111827'
+      }}
+    >
       <Sidebar />
       <div
         className="flex flex-col flex-1 md:ml-64"
         style={{ overflowX: "hidden" }}
-      >
+      >  
         <main className="flex-1 p-4 md:p-6 lg:p-8">
           <Outlet />
         </main>
